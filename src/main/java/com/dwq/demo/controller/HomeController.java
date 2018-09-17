4c643ad2f3301d22210eb64157c2f06c3ac0e864
beer <beerdwq95@gmail.com>
2018-09-17 16:21:51 +0800

    Initial commit

---
+++ src/main/java/com/dwq/demo/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.dwq.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "1";
+    }
+}
